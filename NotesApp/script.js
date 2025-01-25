f17dc295eb56d11afb129e35b1dc860373beebf7
Shishu Raj <106923546+Craaaj@users.noreply.github.com>
2025-01-25 18:08:07 +0530

    Update script.js

--- NotesApp/script.js
+++ NotesApp/script.js
@@ -18,7 +18,6 @@ createBtn.addEventListener("click", ()=>{
     let img = document.createElement("img");
     inputBox.className = "input-box";
     inputBox.setAttribute("contenteditable", true);
-    // img.setAttribute("id","dltbtn")
     img.src = "images/delete.png";
     notesContainer.appendChild(inputBox).appendChild(img);
 });
@@ -48,45 +47,3 @@ document.addEventListener("keydown", event =>{
     }
 });
 
-
-
-
-
-
-
-
-
-
-// // Function to add image to all existing input boxes
-// function addImageToInputBoxes() {
-//     var elements = document.getElementsByClassName('input-box');
-//     for (let i = 0; i < elements.length; i++) {
-//         // Create the image element
-//         let img = document.createElement("img");
-//         img.src = "images/delete.png";
-        
-//         // Append the image to the existing input box
-//         elements[i].appendChild(img);
-//     }
-// }
-
-// // Event listener for the create button
-// createBtn.addEventListener("click", () => {
-//     // Create a new input box
-//     let inputBox = document.createElement("p");
-//     inputBox.className = "input-box";
-//     inputBox.setAttribute("contenteditable", true);
-
-//     // Create the image for the new input box
-//     let img = document.createElement("img");
-//     img.src = "images/delete.png";
-    
-//     // Append the image to the new input box
-//     inputBox.appendChild(img);
-    
-//     // Append the new input box to the notes container
-//     notesContainer.appendChild(inputBox);
-
-//     // Call the function to add images to all input boxes
-//     addImageToInputBoxes();
-// });
